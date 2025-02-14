4f9d70101312758c9518cbc8bc46c20e6581ae0e
mdarbyy <michaeldarby2698@gmail.com>
2025-02-14 10:09:32 -0500

    Initial Commit
    - created app
    - implemented devise with user model
    - designed sign in page and users routes

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users'
+  }
+
+  authenticated :user do
+    resources :settings
+    resources :users
+    get 'static_pages/home', to: 'static_pages#home', as: 'home'
+  end
+
+  root "static_pages#home"
+end
