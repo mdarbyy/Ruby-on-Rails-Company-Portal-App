d3511eee625f4e97f5acaa4bc3af85521359d728
mdarbyy <michaeldarby2698@gmail.com>
2025-03-03 09:53:08 -0500

    Alternate way to create users
    - created my own registrations controller to show custom flash messages
    - created custom route to create users when a user is logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users'
+    registrations: 'users/registrations'
   }
 
   authenticated :user do
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
 
     resources :settings
     resources :users
+
+    post 'admin/users', to: 'users#create', as: 'admin_create_user'
+
     get 'static_pages/employee_information', to: 'static_pages#employee_information', as: 'employee_information'
     get 'static_pages/home', to: 'static_pages#home', as: 'home'
   end
